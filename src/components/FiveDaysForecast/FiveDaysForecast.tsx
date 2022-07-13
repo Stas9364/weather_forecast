12164dde17296b760cdc94a414fe950eea367806
Stas9364 <stas-lisovskii@mail.ru>
2022-07-13 15:22:45 +0300

    test

--- src/components/FiveDaysForecast/FiveDaysForecast.tsx
+++ src/components/FiveDaysForecast/FiveDaysForecast.tsx
@@ -4,7 +4,7 @@ import {useAppSelector} from '../../App/app/hooks';
 export const FiveDaysForecast = () => {
     const forecast = useAppSelector(state => state.fiveDaysForecast.list);
 
-    console.log(forecast)
+    console.log('TEST BRANCH')
 
     // const forecastList = forecast.map((el, index) => <li key={index}>{el.main.temp}</li>);
 
