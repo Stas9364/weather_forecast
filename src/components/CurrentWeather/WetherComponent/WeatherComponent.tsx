22bf80e1174b35b3f5a848d685a7face6982707b
Stas9364 <stas-lisovskii@mail.ru>
2022-07-17 18:08:09 +0300

    commit

--- src/components/CurrentWeather/WetherComponent/WeatherComponent.tsx
+++ src/components/CurrentWeather/WetherComponent/WeatherComponent.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import {ResponseCurrentWeatherType} from '../../../api/apiReaquests';
 
 export const WeatherComponent = (data: ResponseCurrentWeatherType) => {
-    console.log(data)
     return (
-        <>
-            CURRENT WEATHER
-        </>
+        <div>
+            <div>{data.location.name}</div>
+            <div>{data.current.last_updated}</div>
+            <div>Description: {data.current.condition.text}</div>
+            <div><img src={data.current.condition.icon} alt="showing pic"/></div>
+            <div>Temperature: {data.current.temp_c} C&deg;</div>
+            <div>Feels like: {data.current.feelslike_c} C&deg;</div>
+            <div>Wind speed: {data.current.wind_kph} km/h</div>
+            <div>Wind direction: {data.current.wind_dir}</div>
+            <div>Pressure: {data.current.pressure_mb}</div>
+            <div>Humidity: {data.current.humidity} %</div>
+        </div>
     );
 };
